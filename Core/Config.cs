67f649a28e260275f7f0e49c985d3c727e71d89e
bartr <bartr@microsoft.com>
2021-02-05 00:49:04 -0600

    added DataService, Cache and Port

--- Core/Config.cs
+++ Core/Config.cs
@@ -23,14 +23,17 @@ namespace Ngsa.DataService
     public class Config
     {
         public AppType AppType { get; set; } = AppType.DataService;
+        public string DataService { get; set; } = string.Empty;
         public string SecretsVolume { get; set; } = "secrets";
         public LogLevel LogLevel { get; set; } = LogLevel.Warning;
         public bool DryRun { get; set; }
         public bool InMemory { get; set; }
+        public bool Cache { get { return !NoCache; } }
         public bool NoCache { get; set; }
         public int PerfCache { get; set; }
         public int CacheDuration { get; set; } = 300;
         public string Zone { get; set; } = string.Empty;
         public string Region { get; set; } = string.Empty;
+        public int Port { get; set; } = 8080;
     }
 }
