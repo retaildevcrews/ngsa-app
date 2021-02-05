d481f96c862e32100402eb61618330fe775687fe
bartr <bartr@microsoft.com>
2021-02-05 01:12:16 -0600

    linter

--- Core/Config.cs
+++ Core/Config.cs
@@ -1,16 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.CommandLine;
-using System.CommandLine.Parsing;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using Ngsa.Middleware;
 
 namespace Ngsa.DataService
 {
@@ -28,7 +19,7 @@ namespace Ngsa.DataService
         public LogLevel LogLevel { get; set; } = LogLevel.Warning;
         public bool DryRun { get; set; }
         public bool InMemory { get; set; }
-        public bool Cache { get { return !NoCache; } }
+        public bool Cache => !NoCache;
         public bool NoCache { get; set; }
         public int PerfCache { get; set; }
         public int CacheDuration { get; set; } = 300;
